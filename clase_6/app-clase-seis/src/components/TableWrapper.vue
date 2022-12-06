f787008f130d88fe455fceea464801fb864be3cd
federiva <federivadeneira@gmail.com>
2022-12-06 13:00:40 -0300

    subiendo ejercicio 6

---
+++ clase_6/app-clase-seis/src/components/TableWrapper.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1>
+        Ejercicio <a href="https://docs.google.com/presentation/d/1ElWRdhoF2hC7zwYvXUZmcEEICYGC4iQxgI-NDOn-Yko/preview#slide=id.gf86d87a152_0_7" target="_blank">Clase 6</a>
+    </h1>
+    <h3>Tablas</h3>
+    <table-component
+        :data="tableData.arboles.dataTabla"
+        :title="tableData.arboles.tituloTabla"
+        :params="tableData.arboles.tablaParams">
+    ></table-component>
+    <table-component
+            :data="tableData.tablas.dataTabla"
+            :title="tableData.tablas.tituloTabla"
+            :params="tableData.tablas.tablaParams">
+          </table-component>
+          <table-component
+            :data="tableData.principios.dataTabla"
+            :title="tableData.principios.tituloTabla"
+            :params="tableData.principios.tablaParams">
+          </table-component>
+  </div>
+</template>
+
+<script>
+import TableComponent from '@/components/TableComponent.vue';
+import { tableDefinitions } from "@/components/tableDefinitions.js";
+
+export default {
+  name: 'table-wrapper',
+  data: function() {
+    return(
+        {
+            tableData: tableDefinitions
+        }
+    )
+  },
+  components: {
+    TableComponent
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ clase_6/app-clase-seis/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '@/../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '@/../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import '@/../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import '@/assets/css/style.css';
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
